686d3f4f6b794076d60fef092d2273b3e2e068a8
Anastasiia Faleeva <a.s.faleeva@gmail.com>
2025-10-28 21:59:05 +0100

    registration v.1

---
+++ BDAS2_Flowers/Models/ViewModels/AuthVm.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BDAS2_Flowers.Models.ViewModels;
+
+public class RegisterVm
+{
+    [Required, EmailAddress] public string Email { get; set; } = "";
+    [Required, MinLength(6)] public string Password { get; set; } = "";
+    [Required] public string FirstName { get; set; } = "";
+    [Required] public string LastName { get; set; } = "";
+    [Required, RegularExpression(@"^\d{7,15}$", ErrorMessage = "Jen čísla")] public string Phone { get; set; } = "";
+}
+
+public class LoginVm
+{
+    [Required, EmailAddress] public string Email { get; set; } = "";
+    [Required] public string Password { get; set; } = "";
+    public string? ReturnUrl { get; set; }
+}
